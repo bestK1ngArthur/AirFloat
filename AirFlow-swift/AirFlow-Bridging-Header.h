8d7fd9afa9966fe2572dcaab8b8d9fb138ba652e
Artem Belkov <bestk1ngarthur@aol.com>
2019-02-23 14:02:24 +0300

    Add swift target

---
+++ AirFlow-swift/AirFlow-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  AirFlow-Bridging-Header.h
+//  AirFloat
+//
+//  Created by bestK1ng on 23/02/2019.
+//
+
+#ifndef AirFlow_Bridging_Header_h
+#define AirFlow_Bridging_Header_h
+
+#import <Foundation/Foundation.h>
+#import <libairfloat/audiooutput.h>
+
+#endif /* AirFlow_Bridging_Header_h */
---
+++ AirFlow-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirFlow-swift
+//
+//  Created by bestK1ng on 23/02/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
