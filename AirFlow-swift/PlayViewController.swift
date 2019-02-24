c5097410446b997ff7afeb5790e0ecb796c6496c
Artem Belkov <bestk1ngarthur@aol.com>
2019-02-24 13:51:50 +0300

    Add base UI navigation

--- AirFlow-swift/PlayViewController.swift
+++ AirFlow-swift/PlayViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PlayViewController.swift
 //  AirFlow-swift
 //
 //  Created by bestK1ng on 23/02/2019.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PlayViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
